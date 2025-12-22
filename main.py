9ad16a986a53138d95c4a85d9b010235c1d0221c
codingalworkvv-pixel <vivaan.vermaau@gmail.com>
2025-12-22 18:35:54 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello, world")
+print('alpha, beta, gamma')
+print(4)
+print("The result of 8 + 6 is", 8+6) # Adding 8 and 6 together
+
+
+a = "crow"
+b = "thirsty"
+c = "water"
+print("Once there was a", a, "and he was very ", b, "he was looking for", c)
