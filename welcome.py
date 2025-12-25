7bfd47e40e1613a19ae129801a08138702427d74
codingalworkvv-pixel <vivaan.vermaau@gmail.com>
2025-12-25 18:03:14 +1100

    Add files via upload

--- welcome.py
+++ welcome.py
@@ -3,3 +3,20 @@ print("Vivaan", end = " ")
 print("Verma")
 print("Vivaan", "Verma", sep = "+")
 print("My age is", 10)
+"""ALpha, beta, gamma""" # Multi line comment
+  # Variable can store values and can vary
+  
+
+
+
+b1 = "1/2/2018"
+b2 = "2/4/2019"
+b3 = "3/6/2020"
+b4 = "4/8/2021"
+b5 = "5/10/2022"
+
+print(b1)
+print(b2)
+print(b3)
+print(b4)
+print(b5)
