d4fc1a90a77c9156d5f47f26aaff4fd46f829764
codingalworkvv-pixel <vivaan.vermaau@gmail.com>
2026-01-01 18:24:45 +1100

    Add files via upload

---
+++ whileloops(1).py
@@ -0,0 +1,12 @@
+i = 0
+while i<=0:
+    print("I WILL RUN FOREVER! ")
+    i = i+1
+
+n = int(input("Enter the amount of terms you wish to find the sum of: "))
+sum = 0
+i = 1
+while i<=n:
+    sum = sum + i
+    i = i + 1
+print(sum)
